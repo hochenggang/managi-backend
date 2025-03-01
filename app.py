7c909c654f83377ae1bf9e57e391fe59b940080d
hochenggang <imhcg@live.com>
2025-03-01 18:51:32 +0300

    去掉了认证，添加了SSH的WS代理

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from typing import List
-import uuid
 import time
-import sqlite3
-import threading
-import hashlib
-from collections import OrderedDict
 import json
 import io
+import asyncio
 
-from fastapi import FastAPI, Depends, HTTPException, Header, Request, Response
-from fastapi.responses import JSONResponse
+
+from fastapi import (
+    FastAPI,
+    Request,
+    Response,
+    WebSocket,
+)
+from fastapi.responses import HTMLResponse
+from fastapi.websockets import WebSocketState
+
 from pydantic import BaseModel
 import paramiko
 
@@ -17,109 +21,6 @@ import paramiko
 # FastAPI 应用
 app = FastAPI()
 
-
-class Token:
-    '''
-    简易的验证类，用于验证令牌
-    '''
-    def __init__(self):
-        self.tokens = OrderedDict()
-        self.lock_token_operation = threading.Lock()
-
-    def add_token(self, token_id: str, token_info: dict):
-        with self.lock_token_operation:  # 确保线程安全
-            if token_id in self.tokens:
-                # 如果 token_id 已经存在，更新其信息并将其移动到末尾
-                self.tokens.move_to_end(token_id)
-                self.tokens[token_id] = token_info
-            else:
-                # 如果 tokens 的长度大于 100，则删除最旧的 token
-                if len(self.tokens) >= 100:
-                    self.tokens.popitem(last=False)  # 移除最旧的项
-                self.tokens[token_id] = token_info
-                self.tokens.move_to_end(token_id, last=False)  # 将新插入的 token_id 移动到最前面
-
-    def generate_token(self, user_name):
-        token = str(uuid.uuid4())
-        create_at = round(time.time())
-        expire_at = create_at + 3600
-        self.add_token(token, {"user_name": user_name, "create_at":create_at, "expire_at": expire_at})
-        return token
-
-    def verify_token(self, authorization: str = Header(None))->dict:
-        if not authorization or not authorization.startswith("Bearer "):
-            raise HTTPException(status_code=401, detail="Invalid Authorization header")
-        token = authorization.split(" ")[1]
-        with self.lock_token_operation:  # 确保线程安全
-            if token not in self.tokens:
-                raise HTTPException(status_code=401, detail="Invalid or expired token")
-            token_info = self.tokens[token]
-            if time.time() > token_info["expire_at"]:
-                del self.tokens[token]  # 删除过期的 token
-                raise HTTPException(status_code=401, detail="Invalid or expired token")
-            return token_info
-
-
-# 验证类实例
-token_manager = Token()
-
-
-# 数据库连接管理类
-class DatabaseConnection:
-    def __init__(self, db_path):
-        self.db_path = db_path
-        self.lock = threading.Lock()
-
-    def execute_query(self, query, params=None, fetch=False):
-        """
-        执行 SQL 查询，并在操作完成后立即释放连接。
-        :param query: SQL 查询语句
-        :param params: 查询参数
-        :param fetch: 是否获取查询结果
-        :return: 如果 fetch=True，返回查询结果；否则返回 None
-        """
-        if not self.lock.acquire(timeout=5):  # 设置超时时间为 5 秒
-            raise TimeoutError("获取数据库连接超时")
-        try:
-            conn = sqlite3.connect(self.db_path)
-            cursor = conn.cursor()
-            if params:
-                cursor.execute(query, params)
-            else:
-                cursor.execute(query)
-            if fetch:
-                result = cursor.fetchall()
-            else:
-                result = None
-            conn.commit()
-            return result
-        finally:
-            if conn:
-                conn.close()
-            self.lock.release()
-
-
-# 数据库连接实例
-db = DatabaseConnection("management.db")
-
-# 初始化数据库
-def init_db():
-    db.execute_query('''
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password_hash TEXT NOT NULL
-        )
-    ''')
-
-
-# 初始化数据库
-init_db()
-
-# 数据模型
-class User(BaseModel):
-    username: str
-    password: str
 
 class Node(BaseModel):
     name: str
@@ -129,7 +30,7 @@ class Node(BaseModel):
     auth_type: str  # 认证类型：password 或 key
     auth_value: str  # 密码或密钥的值
 
-    
+
 class NodesCmds(BaseModel):
     nodes: List[Node]
     cmds: List[str]
@@ -142,15 +43,13 @@ class CmdsTestResult(BaseModel):
     error: List[str]
     node: Node
     cmds: str
-    
-class TokenInfoResult(BaseModel):
-    username: str
-    create_at: str
-    expire_at: str
+
 
 # SSH 连接类
 class SSHConnection:
-    def __init__(self, ip:str, port:int, ssh_username:str, auth_type:str, auth_value:str):
+    def __init__(
+        self, ip: str, port: int, ssh_username: str, auth_type: str, auth_value: str
+    ):
         self.ip = ip
         self.port = port
         self.ssh_username = ssh_username
@@ -166,11 +65,19 @@ class SSHConnection:
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         if self.auth_type == "password":
             # 使用密码认证
-            self.ssh.connect(self.ip, port=self.port, username=self.ssh_username, password=self.auth_value, timeout=5)
+            self.ssh.connect(
+                self.ip,
+                port=self.port,
+                username=self.ssh_username,
+                password=self.auth_value,
+                timeout=5,
+            )
         elif self.auth_type == "key":
             # 使用密钥认证
             key = paramiko.RSAKey.from_private_key(io.StringIO(self.auth_value))
-            self.ssh.connect(self.ip, port=self.port, username=self.ssh_username, pkey=key, timeout=5)
+            self.ssh.connect(
+                self.ip, port=self.port, username=self.ssh_username, pkey=key, timeout=5
+            )
         else:
             raise ValueError(f"Unsupported auth_type: {self.auth_type}")
         return self.ssh
@@ -183,60 +90,8 @@ class SSHConnection:
             self.ssh.close()
 
 
-# 缓存管理员的数量，用于防止管理员初始化接口被call多次
-cache_admin_user_count = db.execute_query("SELECT COUNT(*) FROM users", fetch=True)[0][0]
-# API 端点
-@app.post("/api/admin/init")
-def create_admin(user: User):
-    '''
-    为第一次访问的人创建管理员账号，初始化数据库，并返回成功信息。
-    :param user: 用户名、密码
-    :return: 成功信息
-    '''
-    global cache_admin_user_count
-    if cache_admin_user_count > 0:
-        return Response(status_code=403, content="Admin already exists")
-    else:
-        # 对用户名和密码进行检测
-        if not all([len(user.username)>=3, len(user.password)>=6]):
-            return Response(status_code=400, content="用户名或者密码太短")
-        
-        password_hash = hashlib.sha256(user.password.encode()).hexdigest()
-        db.execute_query("INSERT INTO users (username, password_hash) VALUES (?, ?)",
-                        (user.username, password_hash))
-        cache_admin_user_count += 1
-        return Response(status_code=200, content="Success")
-
-@app.post("/api/login")
-def login(user: User):
-    '''
-    实现用户登录接口，返回token
-    '''
-    # 这个接口后续需要进行限流，1小时允许3次
-    password_hash = hashlib.sha256(user.password.encode()).hexdigest()
-    result = db.execute_query("SELECT username FROM users WHERE username = ? AND password_hash = ?",
-                              (user.username, password_hash), fetch=True)
-    if not result:
-        return JSONResponse(status_code=401, content={
-            "error": "Unauthorized",
-            "message": "Invalid username or password"
-            })
-    username = result[0][0]
-    token = token_manager.generate_token(username)
-    return {"access_token": token, "token_type": "bearer"}
-
-
-@app.get("/api/token/info")
-def get_api_token_info(token: dict = Depends(token_manager.verify_token))->TokenInfoResult:
-    '''
-    返回token的有效期和用户名
-    '''
-    print(token)
-    return Response(status_code=200, content=json.dumps(token))
-
-
 @app.post("/api/ssh/test")
-def test_ssh_connection(node: Node,cmds: List[str], token: dict = Depends(token_manager.verify_token))->CmdsTestResult:
+def test_ssh_connection(node: Node, cmds: List[str]) -> CmdsTestResult:
     """
     测试 SSH 连接并执行测试命令。
     :param node: 包含节点信息的对象
@@ -245,10 +100,12 @@ def test_ssh_connection(node: Node,cmds: List[str], token: dict = Depends(token_
     start_time = time.time()
     # 将命令列表转换为一个大的命令。备注：整个的命令会在前端按行分割转为列表传输，为未来预留可能的 api 行为变化冗余
     commands = "\n".join(cmds)
-    
+
     try:
         # 使用 SSHConnection 类测试连接并执行命令
-        with SSHConnection(node.ip, node.port, node.ssh_username, node.auth_type, node.auth_value) as ssh:
+        with SSHConnection(
+            node.ip, node.port, node.ssh_username, node.auth_type, node.auth_value
+        ) as ssh:
             # 执行测试命令
             stdin, stdout, stderr = ssh.exec_command(commands)
             output = stdout.readlines()
@@ -256,7 +113,7 @@ def test_ssh_connection(node: Node,cmds: List[str], token: dict = Depends(token_
             success = True
     except Exception as e:
         success = False
-        output = ['']
+        output = [""]
         error = [str(e)]
     end_time = time.time()
 
@@ -266,45 +123,109 @@ def test_ssh_connection(node: Node,cmds: List[str], token: dict = Depends(token_
         "success": success,
         "output": output,
         "error": error,
-        "node":{
-            "name":node.name,
-            "ip":node.ip,
-            "ssh_username":node.ssh_username,
-            "port":node.port,
-            "auth_type":node.auth_type,
-            "auth_value":'***',
+        "node": {
+            "name": node.name,
+            "ip": node.ip,
+            "ssh_username": node.ssh_username,
+            "port": node.port,
+            "auth_type": node.auth_type,
+            "auth_value": "***",
         },
-        "cmds":commands
+        "cmds": commands,
     }
 
 
 @app.get("/api/ping")
 def get_ping():
-    '''
+    """
     测试接口是否正常
     返回初始化状态
-    '''
+    """
     global cache_admin_user_count
     cache_api_ping = {}
-    cache_api_ping['pong'] = cache_admin_user_count
+    cache_api_ping["pong"] = cache_admin_user_count
     return cache_api_ping
+
+
+@app.get("/")
+async def get():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+
+
+# WebSocket 路由
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        # 接收连接信息
+        connection_info = await websocket.receive_text()
+        connection_info = json.loads(connection_info)
+
+        # 解析连接信息
+        host = connection_info.get("host")
+        port = connection_info.get("port", 22)
+        username = connection_info.get("username")
+        password = connection_info.get("password")
+        private_key = connection_info.get("privateKey")
+
+        # 创建 SSH 客户端
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # 认证方式：密钥或密码
+        if private_key:
+            private_key_obj = paramiko.RSAKey.from_private_key_file(private_key)
+            ssh.connect(host, port=port, username=username, pkey=private_key_obj)
+        else:
+            ssh.connect(host, port=port, username=username, password=password)
+
+        # 创建 SSH shell 通道
+        channel = ssh.invoke_shell()
+        channel.setblocking(False)
+
+        # 实时转发数据
+        async def forward_output():
+            while True:
+                if channel.recv_ready():
+                    data = channel.recv(1024).decode("utf-8")
+                    await websocket.send_text(data)
+                await asyncio.sleep(0.1)
+
+        async def forward_input():
+            while True:
+                data = await websocket.receive_text()
+                channel.send(data.encode("utf-8"))
+
+        # 运行任务
+        await asyncio.gather(forward_output(), forward_input())
+
+    except Exception as e:
+        await websocket.send_text(f"SSH Error: {str(e)}")
+    finally:
+        if not websocket.client_state == WebSocketState.DISCONNECTED:
+            await websocket.close()
+        ssh.close()
+
 
 # OPTION /*
 @app.options("/{path:path}")
 async def options_handler(path: str):
-    '''
+    """
     跨域请求处理
-    '''
+    """
     return Response(status_code=204)
+
 
 # 添加 CORS 头
 @app.middleware("http")
 async def add_cors_headers(request: Request, call_next):
     response = await call_next(request)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
-    response.headers['X-VERSION'] = "20241230"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET,PUT,POST,DELETE,OPTIONS"
+    response.headers["X-VERSION"] = "20241230"
     return response
 
 
@@ -312,10 +233,12 @@ async def add_cors_headers(request: Request, call_next):
 if __name__ == "__main__":
     import argparse
     import uvicorn
+
     parser = argparse.ArgumentParser(description="Run the FastAPI application.")
-    parser.add_argument("-p", "--port", type=int, default=18001, help="Port to run the application on.")
-    
+    parser.add_argument(
+        "-p", "--port", type=int, default=18001, help="Port to run the application on."
+    )
+
     # 解析命令行参数
     args = parser.parse_args()
     uvicorn.run(app, host="0.0.0.0", port=args.port)
-
